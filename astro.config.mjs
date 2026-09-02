aeb537815546a435a8de72a8c877a8263d082461
coolswood <coolswood@yandex.ru>
2026-09-02 14:41:05 +0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,25 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://logrus.fi',
   compressHTML: true,
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/404'),
+      i18n: {
+        defaultLocale: 'ru',
+        locales: {
+          ru: 'ru-RU',
+          en: 'en-US',
+          fi: 'fi-FI',
+        },
+      },
+    }),
+  ],
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'hover',
